f32f13ce240f5a0aa489f242aaaf9b34a23a0057
Valter J <valterj.14s@gmail.com>
2022-02-11 10:31:53 -0300

    evolucao dos projetos da baixa

---
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -0,0 +1,3 @@
+public enum Desempenho {
+    A_DESEJAR;
+}
---
+++ collectionsJava/src/main/java/CursoInt.java
@@ -0,0 +1,12 @@
+public class CursoIntJava8 {
+
+    private String nome;
+    private int alunos;
+
+    public CursoIntJava8(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    
+}
